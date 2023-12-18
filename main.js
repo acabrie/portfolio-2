f6034dd379b75b4ef785b747329218652e64c801
acabrie <102576895+acabrie@users.noreply.github.com>
2023-12-18 16:11:53 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Toggle mobile navigation
+
+function toggleNav(){
+    const navigation = document.querySelector('.primary-nav')
+    const navToggle = document.querySelector('.hamburger-icon')
+
+    navigation.classList.toggle('open')
+    navToggle.classList.toggle('open')
+}
